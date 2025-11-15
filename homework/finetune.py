31a2cbe919b6207cc371350e46c68333a8f97cd7
PM <13983935+ptMcGit@users.noreply.github.com>
2025-11-15 11:13:33 -0700

    adjust finetune hyperparams

--- homework/finetune.py
+++ homework/finetune.py
@@ -114,11 +114,11 @@ def train(
     train_dataset_name: str = "train",
     output_dir: str = "vlm_model",
     num_train_epochs: int = 0.05,  # use only 0.05 epoch for training
-    per_device_train_batch_size: int = 16,
+    per_device_train_batch_size: int = 8,
     gradient_accumulation_steps: int = 4,
     learning_rate: float = 5e-4,
-    lora_r: int = 8,
-    lora_alpha: int = 32,
+    lora_r: int = 16,
+    lora_alpha: int = 64,  # should be 4-5x lora_r
     lora_dropout: float = 0.0,
     num_workers: int = 16,
 ):
