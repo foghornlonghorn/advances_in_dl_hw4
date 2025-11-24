df51e38f13ef75f7c94a2e1da9627b9682bb52b4
PM <13983935+ptMcGit@users.noreply.github.com>
2025-11-24 08:16:21 -0700

    update finetune

--- homework/finetune.py
+++ homework/finetune.py
@@ -116,11 +116,11 @@ def train(
     num_train_epochs: int = 0.05,  # use only 0.05 epoch for training
     per_device_train_batch_size: int = 8,
     gradient_accumulation_steps: int = 4,
-    learning_rate: float = 1e-3,
-    lora_r: int = 16,
-    lora_alpha: int = 64,  # should be 4-5x lora_r
+    learning_rate: float = 5e-4,
+    lora_r: int = 8,
+    lora_alpha: int = 32,  # should be 4-5x lora_r
     lora_dropout: float = 0.0,
-    num_workers: int = 16,
+    num_workers: int = 2,
 ):
     """
     Fine-tune a VLM model using LoRA.
